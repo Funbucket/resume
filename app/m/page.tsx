b4f4299c11c04e58535375bdb4892b113e0f5e4b
조해창 <outofchng@gmail.com>
2023-11-24 00:55:52 +0900

    수정

--- app/m/page.tsx
+++ app/m/page.tsx
@@ -7,26 +7,26 @@ export default function Home() {
         <Section px="8" py="0">
           <Flex direction="column" align="center">
             <Container pb="1">
-              <Heading size="5">조해창</Heading>
+              <Heading size="3">조해창</Heading>
             </Container>
 
             <Container py="0">
-              <Heading size="3" color="gray">
+              <Heading size="2" color="gray">
                 데이터 분석가
               </Heading>
             </Container>
 
             <Flex pb="4" align="center" gap="2">
               <Link color="gray" target="_blank" href="mailto:outofchng@gmail.com">
-                <Text size="2">Email</Text>
+                <Text size="1">Email</Text>
               </Link>
               <Separator orientation="vertical" color="gray" />
               <Link color="gray" target="_blank" href="https://github.com/Funbucket">
-                <Text size="2">Github</Text>
+                <Text size="1">Github</Text>
               </Link>
             </Flex>
 
-            <Text size="2">
+            <Text size="1">
               사회적 가치를 창출하는 서비스가 성장할 수 있는 세상을 만들어 모든 사람이 차별 없이 편리함을 누리는 것을
               꿈꾸고 있습니다. 사용자가 남긴 데이터를 통해 사용자의 마음을 읽어, 편리하고 즐겁게 사용할 수 있는 서비스를
               만들고자 합니다. 데이터를 통해 서비스가 해당 분야의 최고 서비스가 될 수 있는 성장 전략을 수립합니다.
@@ -35,7 +35,7 @@ export default function Home() {
         </Section>
 
         <Section px="8" pb="0">
-          <Heading size="3">SKILL</Heading>
+          <Heading size="2">SKILL</Heading>
 
           <Flex pt="2" pl="4" gap="2">
             <Badge color="gray" variant="outline">
@@ -54,11 +54,11 @@ export default function Home() {
         </Section>
 
         <Section px="8" pt="8" pb="0">
-          <Heading size="3">PROJECTS</Heading>
+          <Heading size="2">PROJECTS</Heading>
 
           <Flex direction="column" pt="2" pl="4" gap="3">
-            <Flex align="center" gap="2">
-              <Text size="2" weight="medium">
+            <Flex direction="column" gap="2">
+              <Text size="1" weight="medium">
                 - 온라인 중고책의 다나와를 위한 여정, 싸다알라딘 사용자 행동 분석 및 유입 채널 분석
               </Text>
 
@@ -68,8 +68,9 @@ export default function Home() {
                 </Button>
               </Link>
             </Flex>
-            <Flex align="center" gap="2">
-              <Text size="2" weight="medium">
+
+            <Flex direction="column" gap="2">
+              <Text size="1" weight="medium">
                 - 친구 수 2300명, DAU 300명 츠누봇의 성장 스토리
               </Text>
               <Link target="_blank" href="https://ouchc.notion.site/5bb7a59f344b4502986e038ab48a8b0f?pvs=4">
@@ -82,27 +83,27 @@ export default function Home() {
         </Section>
 
         <Section px="8" pt="8" pb="0">
-          <Heading size="3">WORK EXPERIENCE</Heading>
+          <Heading size="2">WORK EXPERIENCE</Heading>
 
           <Flex direction="column" pt="2" pl="4" gap="3">
             <Flex direction="column" pt="1">
               <Flex align="center" gap="2">
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   - Intern at KAIST DxD Lab
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   데이터 수집 및 시각화
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   2023.9 - 2023.11
                 </Text>
               </Flex>
 
               <Flex direction="column" pl="3">
                 <Link target="_blank" href="https://ouchc.notion.site/b93fd109a7f444c3b194d869d51c8ccc?pvs=4">
-                  <Text size="2" weight="medium" color="gray">
+                  <Text as="p" size="1" weight="medium" color="gray">
                     - 웨딩, 바디프로필을 찍는 사람이 커뮤니티에서 얻는 왜곡된 인지 현상을 분석하기 위한 비정형 데이터
                     수집 및 분석
                   </Text>
@@ -112,22 +113,22 @@ export default function Home() {
 
             <Flex direction="column" pt="1">
               <Flex align="center" gap="2">
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   - Intern at TOVDATA
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   UX Research
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   2023.6 - 2023.9
                 </Text>
               </Flex>
 
               <Flex direction="column" pl="3">
                 <Link target="_blank" href="https://ouchc.notion.site/UX-abb483fedc3346fbae82a79b844cd4e9?pvs=4">
-                  <Text size="2" weight="medium" color="gray">
+                  <Text as="p" size="1" weight="medium" color="gray">
                     - 개인정보 관련 문서 관리 서비스 Plip의 새로운 유료화 지점 도출을 위한 정성적 데이터 수집
                   </Text>
                 </Link>
@@ -136,27 +137,27 @@ export default function Home() {
 
             <Flex direction="column" pt="1">
               <Flex align="center" gap="2">
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   - Intern at TOVDATA
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   프론트엔드 개발
                 </Text>
                 <Separator orientation="vertical" color="gray" />
-                <Text size="2" weight="medium">
+                <Text size="1" weight="medium">
                   2023.3 - 2023.6
                 </Text>
               </Flex>
 
               <Flex direction="column" pl="3">
                 <Link target="_blank" href="http://tovdata.com/doc/PrivacyDAM%20Manager%20guide.pdf">
-                  <Text size="2" weight="medium" color="gray">
+                  <Text as="p" size="1" weight="medium" color="gray">
                     - PrivacyDam 서비스를 위한 프론트엔드 단독 작업
                   </Text>
                 </Link>
 
-                <Text size="2" weight="medium" color="gray">
+                <Text as="p" size="1" weight="medium" color="gray">
                   - 기존 프론트엔드의 Atomic 구조에서 DX 개선을 위해 DDD를 도입
                 </Text>
               </Flex>
@@ -165,33 +166,33 @@ export default function Home() {
         </Section>
 
         <Section px="8" pt="8" pb="0">
-          <Heading size="3">TEACHING EXPERIENCE</Heading>
+          <Heading size="2">TEACHING EXPERIENCE</Heading>
 
           <Flex direction="column" pt="2" pl="4" gap="3">
             <Flex align="center" gap="2">
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 - SW 교양 과목
               </Text>
               <Separator orientation="vertical" color="gray" />
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 컴퓨터 과학적 사고 파이썬 실습 보조
               </Text>
               <Separator orientation="vertical" color="gray" />
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 2023.3 - 2023.6
               </Text>
             </Flex>
 
             <Flex align="center" gap="2">
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 - SW 교양 과목
               </Text>
               <Separator orientation="vertical" color="gray" />
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 문제 해결을 위한 코딩 기초 파이썬 실습 보조
               </Text>
               <Separator orientation="vertical" color="gray" />
-              <Text size="2" weight="medium">
+              <Text size="1" weight="medium">
                 2022.9 - 2022.12
               </Text>
             </Flex>
---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse, userAgent } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+  const { device } = userAgent(request);
+  const isMobileDevice = device.type === "mobile";
+
+  if (isMobileDevice) {
+    return NextResponse.rewrite(new URL(`/m${url.pathname}${url.search}`, url.origin));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/m", "/((?!api|_next/static|_next/image|favicon.ico|svg).*)"],
+};
