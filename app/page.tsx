71023b5d0e8d0195f0fadcb6dd4eac31a65d0d38
조해창 <outofchng@gmail.com>
2023-11-23 17:50:19 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,156 @@
-import Image from 'next/image'
+import { Badge, Button, Container, Flex, Heading, Link, Section, Separator, Text } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <Section px="8" pb="0">
+        <Flex direction="column" align="center">
+          <Container pb="1">
+            <Heading size="5">조해창</Heading>
+          </Container>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <Container pb="4">
+            <Heading size="3" color="gray">
+              데이터 분석가
+            </Heading>
+          </Container>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Text size="2">
+            사회적 가치를 창출하는 서비스가 성장할 수 있는 세상을 만들어 모든 사람이 차별 없이 편리함을 누리는 것을
+            꿈꾸고 있습니다. 사용자가 남긴 데이터를 통해 사용자의 마음을 읽어, 편리하고 즐겁게 사용할 수 있는 서비스를
+            만들고자 합니다. 데이터를 통해 서비스가 해당 분야의 최고 서비스가 될 수 있는 성장 전략을 수립합니다.
+          </Text>
+        </Flex>
+      </Section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <Section px="8" pb="0">
+        <Heading size="3">SKILL</Heading>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <Flex pt="2" pl="4" gap="2">
+          <Badge color="gray" variant="outline">
+            GA4
+          </Badge>
+          <Badge color="gray" variant="outline">
+            SQL
+          </Badge>
+          <Badge color="gray" variant="outline">
+            BigQuery
+          </Badge>
+          <Badge color="gray" variant="outline">
+            Python
+          </Badge>
+        </Flex>
+      </Section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Section px="8" pt="8" pb="0">
+        <Heading size="3">PROJECTS</Heading>
+
+        <Flex direction="column" pt="2" pl="4" gap="3">
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - 싸다알라딘 사용자 행동 분석 및 유입 채널 분석
+            </Text>
+
+            <Link target="_blank" href="https://ouchc.notion.site/afa09a5a74164e13ad494eb846dd5fe4?pvs=4">
+              <Button color="gray" size="1">
+                자세히 보기
+              </Button>
+            </Link>
+          </Flex>
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - 츠누봇 학식 조회 퍼널 개선기
+            </Text>
+            <Link target="_blank" href="https://ouchc.notion.site/5bb7a59f344b4502986e038ab48a8b0f?pvs=4">
+              <Button color="gray" size="1">
+                자세히 보기
+              </Button>
+            </Link>
+          </Flex>
+        </Flex>
+      </Section>
+
+      <Section px="8" pt="8" pb="0">
+        <Heading size="3">WORK EXPERIENCE</Heading>
+
+        <Flex direction="column" pt="2" pl="4" gap="3">
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - Intern at KAIST DxD Lab
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              데이터 수집 및 시각화
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              2023.9 - 2023.11
+            </Text>
+          </Flex>
+
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - Intern at TOVDATA
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              UX Research
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              2023.6 - 2023.9
+            </Text>
+          </Flex>
+
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - Intern at TOVDATA
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              프론트엔드 개발
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              2023.3 - 2023.6
+            </Text>
+          </Flex>
+        </Flex>
+      </Section>
+
+      <Section px="8" pt="8" pb="0">
+        <Heading size="3">TEACHING EXPERIENCE</Heading>
+
+        <Flex direction="column" pt="2" pl="4" gap="3">
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - SW 교양 과목
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              컴퓨터 과학적 사고 파이썬 실습 보조
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              2023.3 - 2023.6
+            </Text>
+          </Flex>
+
+          <Flex align="center" gap="2">
+            <Text size="2" weight="medium">
+              - SW 교양 과목
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              문제 해결을 위한 코딩 기초 파이썬 실습 보조
+            </Text>
+            <Separator orientation="vertical" color="gray" />
+            <Text size="2" weight="medium">
+              2022.9 - 2022.12
+            </Text>
+          </Flex>
+        </Flex>
+      </Section>
     </main>
-  )
+  );
 }
